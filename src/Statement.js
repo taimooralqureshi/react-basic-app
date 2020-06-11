3e015c0741e1bba9b10b8a9589c94d93dfab55e1
Taimoor Qureshi <taimooralqureshi@gmail.com>
2020-06-11 20:33:53 +0500

    warning refactor

--- src/Statement.js
+++ src/Statement.js
@@ -3,12 +3,11 @@ import React from 'react'
 function Statement({ statement }) {
     {
         return (
-            <div>{statement  &&
+            statement  && 
                 <div className="bg-white display-ib rounded p">
                     <strong>{statement}</strong>
                 </div>
-            }
-            </div>
+            
         )
     }
 
